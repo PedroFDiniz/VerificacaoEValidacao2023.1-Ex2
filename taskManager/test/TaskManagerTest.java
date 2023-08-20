f0368f90c7715e47ef97a3e42c19289b6c67cb33
GabrielCLL <gabriel.cavalcanti.lima@ccc.ufcg.edu.br>
2023-08-20 13:51:07 -0300

    creation of test prototypes for TaskManager

---
+++ taskManager/test/TaskManagerTest.java
@@ -0,0 +1,31 @@
+package taskManager.test;
+
+import org.junit.Test;
+
+public class TaskManagerTest {
+
+    @Test
+    public void testCreateTask() {
+        //TODO
+    }
+
+    @Test
+    public void testUpdateTask() {
+        //TODO
+    }
+
+    @Test
+    public void testDeleteTask() {
+       //TODO
+    }
+
+    @Test
+    public void testListTasks() {
+       //TODO
+    }
+
+    @Test
+    public void testChangePriority() {
+        //TODO
+    }
+}
