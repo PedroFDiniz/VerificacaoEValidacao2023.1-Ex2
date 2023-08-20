84e9b984c1b47c6f9aa2d0a3d81c23ca1aea8739
GabrielCLL <gabriel.cavalcanti.lima@ccc.ufcg.edu.br>
2023-08-20 15:33:09 -0300

    priority enum test

---
+++ taskManager/test/PriorityTest.java
@@ -0,0 +1,23 @@
+package taskManager.test;
+
+import org.junit.jupiter.api.Test;
+import taskManager.main.Priority;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PriorityTest {
+    @Test
+    public void testPriorityEnum() {
+        Priority high = Priority.HIGH;
+        Priority medium = Priority.MEDIUM;
+        Priority low = Priority.LOW;
+
+        assertEquals(0, high.ordinal());
+        assertEquals(1, medium.ordinal());
+        assertEquals(2, low.ordinal());
+
+        assertEquals("HIGH", high.toString());
+        assertEquals("MEDIUM", medium.toString());
+        assertEquals("LOW", low.toString());
+    }
+}
